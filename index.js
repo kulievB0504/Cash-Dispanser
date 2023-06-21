eebc021d516ec9cf2be3e0429b5e767d950a13c6
kulievB0504 <kbekzod2005@gmail.com>
2023-06-21 16:58:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let name = prompt('Как вас зовут?')
+
+
+
+switch (name) {
+    case 'Alex':
+        let account = prompt('Номер счета?')
+        switch (account) {
+            case '7777':
+                let money = prompt('Сколько обналичить?');
+                if (money <= 10000) {
+                    alert('Все отлично. Вы обналичили' + ' ' + money)
+                    let substruction = 10000 - money;
+                    alert(`У вас осталось ${substruction}`);
+                } else {
+                    alert('Недостаточно средств.')
+                }
+                break;
+
+            default:
+                alert(`Обнаружена попытка взлома! IP злоумышоинника занесено в базу данных! Вам *****)`)
+
+
+        }
+        break;
+    default:
+        alert(`Пользователь не найден. Досвидули).`)
+}
+
+
+
+let name_inClub = prompt('Добро подаловать в наш клуб. Будут приняты только тех, чьи имена начинается на А')
+
